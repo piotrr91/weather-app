9682f892f274de6ae65816a43507823e87a911ed
piotrr91 <piotr9116@gmail.com>
2019-05-24 22:49:15 +0200

    Add working version for input with city

--- src/components/App.js
+++ src/components/App.js
@@ -2,24 +2,53 @@ import React, {Component} from 'react';
 import './App.css';
 import Form from './Form';
 import Result from './Result';
+const APIKey = '89a828a6427dbfe875625255fc1014e1';
 
 class App extends Component {
 
   state = {
-    value: ""
+    value: '',
+    temp: '',
+    city: '',
+    date: '',
+    pressure: '',
+    wind: '',
+    sunrise: '',
+    sunset: '',
+    er: '',
   }
 
-  handleInputChange = (e) => {
+  handleInputChange = e => {
     this.setState({
       value: e.target.value
     })
+  }
+  handleCitySubmit = e => {
+    e.preventDefault()
+    const API = 
+    `http://api.openweathermap.org/data/2.5/weather?q=${this.state.value}&APPID=${APIKey}&units=metric`;
+    fetch(API)
+    .then(response => {
+      if(response.ok) {
+        return response
+      }
+      throw Error("fail")
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(er => console.log(er))
+
+
+
   }
   render() {
   return (
     <div className="App">
       Pogoda w twoim mieście
-      <Form value = {this.state.value} change=
-      {this.handleInputChange} />
+      <Form value = {this.state.value} 
+            change = {this.handleInputChange}
+            submit = {this.handleCitySubmit} 
+      />
       <Result />
     </div>
   );
