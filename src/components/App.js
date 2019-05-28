86dafe89d4612116eb83446001ed5c434fb72f0a
piotrr91 <piotr9116@gmail.com>
2019-05-28 23:16:51 +0200

    update state

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import Form from './Form';
 import Result from './Result';
-const APIKey = '89a828a6427dbfe875625255fc1014e1';
+const APIKey = '89a828a6427dbfe875625255fc1014e1'; //klucz do api
 
 class App extends Component {
 
@@ -15,7 +15,7 @@ class App extends Component {
     wind: '',
     sunrise: '',
     sunset: '',
-    er: '',
+    err: false,
   }
 
   handleInputChange = e => {
@@ -35,8 +35,26 @@ class App extends Component {
       throw Error("fail")
     })
     .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(er => console.log(er))
+    .then(data => {
+      const time = new Date().toLocaleString()
+      this.setState(state =>({
+        err: false,
+        temp: data.main.temp,
+        city: this.state.value,
+        date: time,
+        pressure: data.main.pressure,
+        wind: data.wind.speed,
+        sunrise: data.sys.sunrise,
+        sunset: data.sys.sunset,
+      }))
+    })
+    .catch(err => {
+      console.log(err);
+      this.setState(prevState =>({
+        err: true,
+        city: prevState.value
+      }))
+    })
 
 
 
@@ -49,7 +67,7 @@ class App extends Component {
             change = {this.handleInputChange}
             submit = {this.handleCitySubmit} 
       />
-      <Result />
+      <Result weather={this.state} />
     </div>
   );
 }
--- src/components/Result.js
+++ src/components/Result.js
@@ -1,8 +1,37 @@
 import React from 'react';
 
-const Result = () => {
+const Result = props => {
+
+    const { temp, city, date, pressure, wind, sunrise, sunset, err} = props.weather;
+
+    let content = null;
+
+    if(!err && city) {
+
+        const sunriseTime = new Date(sunrise * 1000).toLocaleTimeString();
+        const sunsetTime = new Date(sunset * 1000).toLocaleTimeString(); 
+        content = (
+            <div>
+                <h2>Informacje pogodowe dla: {city}</h2>
+                <h4>Dane dla dnia i godziny: {date}</h4>
+                <h4>Temperatura: {temp} &#176;C</h4>
+                <h4>Ciśnienie: {pressure} hPA</h4>
+                <h4>Siła wiatru: {wind} m/s</h4>
+                <h4>Wschód słońca o {sunriseTime}</h4>
+                <h4>Zachód słońca o {sunsetTime}</h4>
+
+             </div>
+        )
+    }
+
+
     return ( 
-        <div>wynik</div>
+      
+      <div className= "result">
+          {err ? `Nie ma w bazie takiego miasta${city} `: content}
+      </div>
+
+     
      );
 }
  
